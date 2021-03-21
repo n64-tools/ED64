1471eb6a0aa3e28aa348b61209430e234ba02926
Robin Jones <robin@networkfusion.co.uk>
2021-03-21 00:31:06 +0000

    One more (really!)

--- ED64-XIO/src/rtc.c
+++ ED64-XIO/src/rtc.c
@@ -1,7 +1,8 @@
 
 #include "main.h"
 
-extern static volatile struct SI_regs_s * const SI_regs; // = (struct SI_regs_s *) 0xa4800000;
+extern SI_regs_s* const SI_regs;
+//extern static volatile struct SI_regs_s * const SI_regs = (struct SI_regs_s *) 0xa4800000;
 static void * const PIF_RAM = (void *) 0x1fc007c0;
 /** @brief SI DMA busy */
 #define SI_STATUS_DMA_BUSY  ( 1 << 0 )
